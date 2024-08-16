28dc9c731187cfcda5afa32d526efc99b84a69e1
Salomon <158000174+salomoncloud@users.noreply.github.com>
2024-08-15 21:10:22 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "location" {
+  type = string
+  default = "Canada East"
+}
